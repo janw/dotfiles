74182fbb9804255e001c640192d316313a09a8f9
Jan Willhaus <mail@janwillhaus.de>
2015-07-24 13:44:26 +0200

    update install script to symlink into homedir

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Change directory into the repository if called from elsewhere
 cd "$(dirname "$0")"
 
+# Update all submodules 
 git submodule init && git submodule update
+
+ln -s .zshrc     $HOME
+ln -s .zsh       $HOME
+ln -s .oh-my-zsh $HOME
+ln -s .vim       $HOME
+ln -s .vimrc     $HOME
+
 
 cd .awesome-terminal-fonts
 ./install.sh
