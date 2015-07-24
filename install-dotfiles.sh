d0e09448a13e01ef67141f14eddd2413621f9b32
Jan Willhaus <mail@janwillhaus.de>
2015-07-24 14:07:21 +0200

    Added more robust symlinks

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -2,16 +2,17 @@
 
 # Change directory into the repository if called from elsewhere
 cd "$(dirname "$0")"
+DOTDIR=$(pwd)
 
 # Update all submodules 
 git submodule init && git submodule update
 
-ln -s .zshrc     $HOME
-ln -s .zsh       $HOME
-ln -s .oh-my-zsh $HOME
-ln -s .vim       $HOME
-ln -s .vimrc     $HOME
-
+# Create symlinks into the home dir to enable usage of the files
+ln -s $DOTDIR/.oh-my-zsh $HOM
+ln -s $DOTDIR/.zshrc     $HOME
+ln -s $DOTDIR/.zsh       $HOME
+ln -s $DOTDIR/.vim       $HOME
+ln -s $DOTDIR/.vimrc     $HOME
 
 cd .awesome-terminal-fonts
 ./install.sh
