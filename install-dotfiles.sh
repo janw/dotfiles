5caf2f86070c0fe92ef5dc55ec341d0fb071b1a7
Jan Willhaus <mail@janwillhaus.de>
2016-03-17 22:41:36 +0100

    Script should not depend on bash for no reason

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # Change directory into the repository if called from elsewhere
 cd "$(dirname "$0")"
 DOTDIR=$(pwd)
 
-# Update all submodules 
+# Update all submodules
 git submodule init && git submodule update
 
 # Create symlinks into the home dir to enable usage of the files
