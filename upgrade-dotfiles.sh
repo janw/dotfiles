deefb05009b49ae2c40ed02ca3ecfbb326b0499d
Jan Willhaus <mail@janwillhaus.de>
2016-05-19 13:22:44 +0200

    Streamlined dotfiles upgrade script

--- upgrade-dotfiles.sh
+++ upgrade-dotfiles.sh
@@ -1,16 +1,5 @@
-#!/bin/sh
 
-# Change directory into the repository if called from elsewhere
 cd "$(dirname "$0")"
-DOTDIR=$(pwd)
-
 echo "Updating Dotfiles. Stashing changes if necessary."
-cd "$DOTDIR"
-git stash
-if git pull --rebase --stat origin master
-then
-  echo  "Your dotfiles have been updated and/or is at the current version."
-else
-  echo "There was an error updating. Try again later?"
-fi
-git stash apply
+git fetch origin master
+git rebase FETCH_HEAD --stat --autostash
