2ae2ea3164b40772716fc0871d4bf70a4ff147e0
Jan Willhaus <mail@janwillhaus.de>
2017-01-23 10:37:16 +0100

    Migrating font settings to awesome (fontconfig)
    Also: I improved the installer script, with proper verbose output, and a
    check for the currently set login shell

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 
 # Change directory into the repository if called from elsewhere
 cd "$(dirname "$0")"
@@ -7,6 +10,7 @@ DOTDIR=$(pwd)
 # Update all submodules
 git submodule init && git submodule update
 
+echo -e "\n\nSymlinking configs, rc files, etc. ..."
 # Create symlinks into the home dir to enable usage of the files
 ln -sf $DOTDIR/.oh-my-zsh $HOME
 ln -sf $DOTDIR/.zshrc     $HOME
@@ -26,22 +30,54 @@ ln -sf $DOTDIR/.Xresources.dark  $HOME
 # Emacs.d directory
 ln -sf $DOTDIR/.emacs.d          $HOME
 
+echo -e "\n\nDownloading additional fonts ..."
+# Download a few new fonts from the web and install them, too.
+wget -nc "https://github.com/tonsky/FiraCode/releases/download/1.204/FiraCode_1.204.zip"
+unzip -n FiraCode_1.204.zip -d firacode 2>&1 > /dev/null
+
+wget -nc "https://github.com/belluzj/fantasque-sans/releases/download/v1.7.1/FantasqueSansMono.zip"
+unzip -n FantasqueSansMono.zip -d fantasquesans 2>&1 > /dev/null
+
+
 # Link font to library and clear font cache for user fonts
 if [ "$(uname)" = "Darwin" ]
 then
+    echo -e "\n\nWe're on macOS."
 
-    ln -sf $DOTDIR/.awesome-terminal-fonts/patched/*.sh $HOME/Library/Fonts/
-    ln -sf $DOTDIR/.awesome-terminal-fonts/patched/*.ttf $HOME/Library/Fonts/
+    echo "Linking fonts ..."
+    ln -sf $DOTDIR/.awesome-terminal-fonts/build/*.sh $HOME/Library/Fonts/
+    ln -sf $DOTDIR/.awesome-terminal-fonts/build/*.ttf $HOME/Library/Fonts/
+    ln -sf $DOTDIR/firacode/otf/*.otf $HOME/Library/Fonts/
+    ln -sf $DOTDIR/fantasquesans/OTF/*.otf $HOME/Library/Fonts/
+
+    echo "Refreshing font cache ..."
     atsutil databases -remove
 
 else
-    mkdir -p $HOME/.local/share/fonts
-    ln -sf $DOTDIR/.awesome-terminal-fonts/patched/*.sh $HOME/.local/share/fonts
-    ln -sf $DOTDIR/.awesome-terminal-fonts/patched/*.ttf $HOME/.local/share/fonts
+    echo -e "\n\nWe're on Linux. "
+    mkdir -p $HOME/.fonts/
+    mkdir -p $HOME/.config/fontconfig/conf.d/
 
-    fc-cache -f
+
+    echo "Linking fonts ..."
+    ln -sf $DOTDIR/.awesome-terminal-fonts/build/*.sh $HOME/.fonts
+    ln -sf $DOTDIR/.awesome-terminal-fonts/build/*.ttf $HOME/.fonts
+    ln -sf $DOTDIR/10-symbols.conf $HOME/.config/fontconfig/conf.d/
+    ln -sf $DOTDIR/firacode/otf/*.otf $HOME/.fonts
+    ln -sf $DOTDIR/fantasquesans/OTF/*.otf $HOME/.fonts
+
+    echo "Refreshing font cache ..."
+    #fc-cache -f
+    #xsudo dpkg-reconfigure fontconfig
+
     echo "Remember to change the console font accordingly!"
 fi
 
-# Finally change shell
-chsh -s $(which zsh)
+SHELLPATH=$(getent passwd $LOGNAME | cut -d: -f7)
+# Finally change shell if necessary
+if [[ $SHELLPATH =~ zsh$ ]]
+then
+    echo "Shell is already zsh. Done."
+else
+    chsh -s $(which zsh)
+fi
