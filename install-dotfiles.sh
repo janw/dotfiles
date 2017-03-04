71ad7b62245620c34565648b1600335a4cb3183f
Jan Willhaus <mail@janwillhaus.de>
2017-03-04 11:09:53 +0100

    Remove git config printout

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -34,8 +34,6 @@ git config --global include.path "${DOTDIR}/gitconfig_global"
 git config --global core.excludesfile "${DOTDIR}/gitignore_global"
 git config --global core.attributesfile "${DOTDIR}/gitattributes_global"
 
-# Show the entire config just to be sure
-git config -l
 
 
 echo -e "\n\nDownloading additional fonts ..."
