549e8aeefb09adda8b24850d2be14f73e9cfc02e
Jan Willhaus <jan.willhaus@cloud.ionos.com>
2020-10-13 13:04:05 +0200

    zsh: Improve autosuggest/complete settings

--- zsh/theming.zsh
+++ zsh/theming.zsh
@@ -1,4 +1,7 @@
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='bg=31,fg=15'
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+export ZSH_HIGHLIGHT_MAXLENGTH=256
 
 bindkey '^[OA' history-substring-search-up
 bindkey '^[OB' history-substring-search-down
