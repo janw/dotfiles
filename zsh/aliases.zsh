7e4a2d9f58dec7d9e2aa36157f556dc963da0334
Jan Willhaus <jan.willhaus@aiven.io>
2021-08-13 08:53:28 +0200

    git: Improve handling of default branch in aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -21,6 +21,13 @@ alias lsdisk='diskutil list'
 alias cask='brew cask'
 
 # Git shortcuts/aliases
+git_janw_default_branch() {
+    # Expects OMZ git.plugin to be enabled for the git_main_branch function to exist
+    default_branch="$(command git config janw.default-branch || git_main_branch)"
+    echo "Default branch: $default_branch" >&2
+    echo "$default_branch"
+}
+
 alias glol='git log --oneline --max-count=15 --decorate' #  overwrites plugin's glol!
 alias gam='git commit --amend '  # overwrites plugin's gam (git am)
 alias gloll="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" #  re-instates plugin's glol!
@@ -29,12 +36,14 @@ alias gbral="git for-each-ref --sort=-committerdate refs/heads/ --format='%(HEAD
 alias grepos="find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '[ -d {}/.git ] && (echo {} && cd {} && git status -s && echo)' \;"
 alias gtl="git tag --format=\"%(align:12)%(color:bold green)%(refname:strip=2)%(end) %(align:12)%(color:red)%(objectname:short)%(end) %(color:reset)%(authordate:short)  (%(authordate:relative))\""
 alias gpu='git pull'
-alias gcom="git checkout master"
-alias gcmp="git checkout master && git pull"
-alias gfrm='git fetch && git rebase origin/master '
-alias gstartb='git checkout master && git pull origin master && git checkout -b'
+alias gcom='git checkout $(git_janw_default_branch) '
+alias gcmp='gcom && git pull'
+alias gfm='git fetch origin $(git_janw_default_branch) '
+alias gfrm='gfm && git rebase origin/$(git_janw_default_branch) '
+alias gstartb='gcom && git pull origin $(git_janw_default_branch) && git checkout -b'
 alias ggrep='git rev-list --all | xargs git grep'
 alias gampf='gam --no-edit && gpf'
+alias gmsg='git log --format=%B -n 1'
 
 # Kubernetes shortcuts
 alias kcontext='kubectl config set-context $(kubectl config current-context) --namespace'
