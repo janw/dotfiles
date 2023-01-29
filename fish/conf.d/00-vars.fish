0d9572522d1c632d7335203ec47f2ff2fdf61b4b
Jan Willhaus <mail@janwillhaus.de>
2023-01-29 23:25:10 +0100

    shell: add initial fish config

---
+++ fish/conf.d/00-vars.fish
@@ -0,0 +1,16 @@
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CACHE_HOME $HOME/.cache
+
+set -gx DOTFILES $HOME/.dotfiles
+set -gx TERM xterm-256color
+
+set -gx EDITOR vim
+set -gx PROJECT_HOME $HOME/repos
+
+set -gx PATH /opt/homebrew/bin $PATH
+set -gx PATH /opt/homebrew/sbin $PATH
+
+# Pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
