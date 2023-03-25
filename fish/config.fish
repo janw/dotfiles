8816dc533e9eb6550fbc666057c84b67d08c1a06
Jan Willhaus <mail@janwillhaus.de>
2023-03-25 18:01:49 +0100

    fish: Adopt starship prompt

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+
+    if command -v starship >/dev/null
+        starship init fish | source
+    end
 end
