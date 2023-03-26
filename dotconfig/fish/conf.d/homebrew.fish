0dabd1e55ef6513063c34452f3dd6fc9161da692
Jan Willhaus <mail@janwillhaus.de>
2023-03-26 13:14:00 +0200

    fish: Minor homebrew adjustments

--- dotconfig/fish/conf.d/homebrew.fish
+++ dotconfig/fish/conf.d/homebrew.fish
@@ -8,14 +8,20 @@ if [ "$platform" = Linux ]
 else if [ "$platform" = Darwin ]
     if [ "$arch" = arm ]
         set HOMEBREW_HOME /opt/homebrew
-        alias oldbrew=/usr/local/bin/brew
     end
 else
     echo "Unsupported platform for homebrew: $platform"
     exit 1
 end
 
+test -e $HOMEBREW_HOME/bin/brew || exit 0
+
 set -gx PATH "$HOMEBREW_HOME/bin" $PATH
+set -gx HOMEBREW_NO_ANALYTICS 1
+
+if [ $HOMEBREW_HOME != /usr/local ] && [ -e /usr/local/bin/brew ]
+    alias oldbrew=/usr/local/bin/brew
+end
 
 if test -d "$HOMEBREW_HOME/share/fish/completions"
     set -gx fish_complete_path $fish_complete_path $HOMEBREW_HOME/share/fish/completions
