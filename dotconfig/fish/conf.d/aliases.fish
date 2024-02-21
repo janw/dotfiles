4521297d72537bd74540cb51c272b4631c825325
Jan Willhaus <mail@janwillhaus.de>
2024-02-21 17:58:51 +0100

    aliases: Update some

--- dotconfig/fish/conf.d/aliases.fish
+++ dotconfig/fish/conf.d/aliases.fish
@@ -1,23 +1,25 @@
-alias vimconf="vim ~/.vimrc"
-alias upgrade_dotfiles="bash $DOTFILES/tools/upgrade.sh"
+alias vimconf "vim ~/.vimrc"
+alias upgrade_dotfiles "bash $DOTFILES/tools/upgrade.sh"
 
 # Some hash shortcuts (shasum required)
-alias sha256='shasum -a256'
-alias sha512='shasum -a512'
-alias sha1='shasum -a1'
+alias sha256 'shasum -a256'
+alias sha512 'shasum -a512'
+alias sha1 'shasum -a1'
 
-alias cask='brew cask'
-alias lsblkk='lsblk -ldo model,size,path,wwn,serial -I8 --sort model'
+alias lsblkk 'lsblk -ldo model,size,path,wwn,serial -I8 --sort model'
 
-alias doco='docker compose'
-alias dockr='docker run --rm -it -w /code -v $PWD:/code'
+alias doco 'docker compose'
+alias dockr 'docker run --rm -it -w /code -v $PWD:/code'
 
-alias flush-dns='sudo killall -HUP mDNSResponder;sudo killall mDNSResponderHelper;sudo dscacheutil -flushcache'
+alias flush-dns 'sudo killall -HUP mDNSResponder;sudo killall mDNSResponderHelper;sudo dscacheutil -flushcache'
 
-alias caff='caffeinate -di'
-alias date-utc='date -uIseconds'
-alias kubens='DEBUG= command kubens '
-alias sops='EDITOR="code -w" command sops '
-alias machine="$PROJECT_HOME/machine/machine"
+alias caff 'caffeinate -di'
+alias date-utc 'date -uIseconds'
+alias kubens 'DEBUG= command kubens '
+alias sops 'EDITOR "code -w" command sops '
+alias machine "$PROJECT_HOME/machine/machine"
 
-alias r="cd $PROJECT_HOME"
+alias r "cd $PROJECT_HOME"
+
+alias dns 'HTTP3=1 RRTYPE=AAAA VERBOSE=1 command dnslookup'
+alias ytd 'yt-dlp -v --remux-video mkv'
