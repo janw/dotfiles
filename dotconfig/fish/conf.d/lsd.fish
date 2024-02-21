34f87b536ea52d6b7462a9218f30228cff73dc80
Jan Willhaus <mail@janwillhaus.de>
2024-02-21 17:48:45 +0100

    lsd: group dirs first

--- dotconfig/fish/conf.d/lsd.fish
+++ dotconfig/fish/conf.d/lsd.fish
@@ -1,7 +1,7 @@
 status --is-interactive || exit
 command -v lsd 1>/dev/null || exit
 
-alias lsd="lsd --date '+%Y-%m-%d %T' "
+alias lsd="lsd --date '+%Y-%m-%d %T' --group-dirs=first"
 alias l='lsd -l'
 alias la='lsd -A'
 alias ll='lsd -lA'
