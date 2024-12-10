8779f59a1ce9e8e2ffd8284f92457664428a022f
Jan Willhaus <mail@janwillhaus.de>
2024-12-10 22:03:14 +0100

    Add custom asciinema handling

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -1,7 +1,12 @@
+set localdir (dirname (status --current-filename))
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+
+    if test "$ASCIINEMA_REC" = "1"
+        set -gx STARSHIP_CONFIG ~/.config/starship-minimal.toml
+    end
 
     if command -v starship >/dev/null
         starship init fish | source
@@ -11,7 +16,6 @@ if status is-interactive
     end
 end
 
-set localdir (dirname (status --current-filename))
 for file in $localdir/local.d/*.fish
     source $file
 end
